c8d2d3cbd670e3a4e172ee237ca225d7b931db99
FilipeG93 <filipe-geraldes@hotmail.com>
2025-04-03 18:24:57 +0100

    added first script to task5

---
+++ hello.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+#isto é usado para fazer comentarios que não vao ser lidos
+
+echo "hello world"
+
+name="jorge"
+
+echo "hello $name"
+
+#tem de ter $ antes do name
+
+read "varname"
+
+echo "hello this is $varname"
+
+echo -e "antonio\njoana\njoao\nfilipe\nmariana"
+
+#tem de ter o -e senao o \n nao vai ser lido como nova linha.
+
+
