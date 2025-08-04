6522e7b2ae48c3b33b0af320e3b0f9436c5d5c1f
FilipeG93 <filipe-geraldes@hotmail.com>
2025-08-04 17:58:02 +0100

    updated bashlogger

--- bashlogger.sh
+++ bashlogger.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
+{
 #datenow=$(date +"%Y-%m-%d %H:%M:%S") duas formas diferentes de fazer o mesmo, nota nos espaços
 datenow= date "+%Y-%m-%d | %H:%M:%S"
 
 #aqui se tiver um espaço entre = e $ nao funciona
-username=$USER
+#username=$USER
 
 #tem de ser parentesis retos senao nao funciona
-echo "[$datenow]  User: $username"
-#echo "$USER" outra forma de fzer o mesmo
-echo "Log entry: Script executed correctly"
+echo "$datenow"
+#echo "[$datenow]  User: $username" na mesma linha outra forma de fazer o mesmo
+echo "User: $USER"
+
+
+echo "difference between > and >> :
+> writes text into a file deleting everything that was previously there
+>> appends text to a file without overwriting what was previously there"
+
+echo "Log entry: Script executed successfully"
 echo "===========================================================" 
+} >> user_activity.log
+ 
 
 
 
