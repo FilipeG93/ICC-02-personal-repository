6dddd343e0ee12e8d8ad5d759c720340c33d6dd9
FilipeG93 <filipe-geraldes@hotmail.com>
2025-08-04 17:08:25 +0100

    bash network tool first version

--- network_tool_v0.sh
+++ network_tool_v0.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-echo "1) Check Network Interface Information
+echo "
+ 
+1) Check Network Interface Information
 2) Ping a Host
 3) Port Scan with Nmap
 4) Display Routing Table
 5) Traceroute to Host
-6) Exit"uiyui
-
+6) Exit"
+ 
 echo "choose an option: "
 
 read varname
@@ -15,20 +17,24 @@ if [ $varname -eq 1 ]; then
 ip a
 
 elif [ $varname -eq 2 ]; then
-read -p "inriduce pig destination" x
-ping $x
+read -p "Enter ping destination: " x
+ping -c 5 $x
 
 elif [ $varname -eq 3 ]; then
-read -p "introduce ip to scan" y
-nmap -sn -Pn $y
+read -p "introduce ip to scan: " y
+nmap -sn -p0 $y
 
 elif [ $varname -eq 4 ]; then
-echo "routing table"
+netstat -rn
 
 elif [ $varname -eq 5 ]; then
-traceroute $host
+read -p "enter host for traceroute: " z
+traceroute $z
+
+elif [ $varname -eq 6 ]; then
+echo "Exiting..."
 
 else
-echo "Exiting..."
+echo "Invalid option. Exiting..."
 
 fi
